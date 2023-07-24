b1ca665f20e5550b7bf7678b6e25f6d94133b0a5
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-07-24 21:06:10 +0530

    Add linter & Create student & teacher rb file

--- Person.rb
+++ Person.rb
@@ -1,25 +1,21 @@
 class Person
-    attr_reader :id
-    attr_accessor :name, :age
-  
-    def initialize(name = "Unknown", age = nil, parent_permission = true)
-      @id = generate_id
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-    def can_use_services?
-        of_age? || @parent_permission
-      end
-    
-      private
-    
-      def of_age?
-        @age.to_i >= 18
-      end
-    
-      def generate_id
-        @id = Random.rand(1..500)
-      end
-    end
-    
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(parent_permission: true, name: 'Unknown', age: nil)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age.to_i >= 18
+  end
+end
