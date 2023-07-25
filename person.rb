a036c1ed7eac0aa234214ff2431fb3be21cf2ff6
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-07-25 18:07:20 +0530

    Create decorator, nameable, capitalize & trimmer file with test

--- person.rb
+++ person.rb
@@ -1,12 +1,19 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(parent_permission: true, name: 'Unknown', age: nil)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
