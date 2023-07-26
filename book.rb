29be46408e13d80725dcaa5c0bfe394fdfaeda13
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-07-26 19:30:22 +0530

    Update required changes by reviewer for rental between book and person

--- book.rb
+++ book.rb
@@ -1,13 +1,16 @@
 class Book
     attr_accessor :title, :author, :rentals
-
-  def initialize(title, author)
-    @title = title
-    @author = author
-    @rentals = []
+  
+    def initialize(title, author)
+      @title = title
+      @author = author
+      @rentals = []
+    end
+  
+    def add_rental(date, person)
+      rental = Rental.new(date, self, person)
+      @rentals << rental
+      rental
+    end
   end
-
-  def add_rental(rental)
-    @rentals << rental
-  end
-end
+  
