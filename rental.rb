f48512804a44cc62419c1acc28138e8bfa183afe
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-07-26 19:35:28 +0530

    Update required changes by reviewer for  association between rental and both book & person

--- rental.rb
+++ rental.rb
@@ -1,10 +1,15 @@
 class Rental
-  attr_accessor :date
-  attr_reader :book, :person
-
-  def initialize(date, book, person)
-    @date = date
-    @book = book
-    @person = person
+    attr_accessor :date
+    attr_reader :book, :person
+  
+    def initialize(date, book, person)
+      @date = date
+      @book = book
+      @person = person
+  
+      @book.add_rental(self)
+  
+      @person.add_rental(self)
+    end
   end
-end
+  
