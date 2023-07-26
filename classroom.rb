ed0f4bc4246a483588248db79f071582f4084923
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-07-26 19:42:47 +0530

    Update required changes by reviewer for classroom  association

--- classroom.rb
+++ classroom.rb
@@ -1,14 +1,15 @@
 class Classroom
-  attr_accessor :label
-  attr_reader :students
-
-  def initialize(label)
-    @label = label
-    @students = []
+    attr_accessor :label
+    attr_reader :students
+  
+    def initialize(label)
+      @label = label
+      @students = []
+    end
+  
+    def add_student(student)
+      student.classroom = self
+      @students << student
+    end
   end
-
-  def add_student(student)
-    student.classroom = self
-    @students << student
-  end
-end
+  
